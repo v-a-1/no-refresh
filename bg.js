eaafdd1a9d6181fdcf95f3459dcb9a59db9af548
v-a-1 <varun.m.ajani@gmail.com>
2012-09-10 21:27:50 +0530

    context menu disable/enable implemented

--- bg.js
+++ bg.js
@@ -9,15 +9,31 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
   current_tab_id = activeInfo.tabId;
 })
 
+// Var mutated by the contextMenu clicks to provide disabling facility.
+var enabled = true;
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.wish_to_make_request == true){
-    if(sender.tab.id == current_tab_id){
+    if(sender.tab.id == current_tab_id && enabled == true){
       sendResponse({make_request: true});
     }else{
       sendResponse({make_request: false});
     }
   }
 });
+
+// Callback for context menu click
+var context_clicked = function(info, tab){
+	enabled = enabled ? false : true;
+	title = enabled ? "Disable Auto Reload on Server Change" : "Enable Auto Reload on Server Change"; 
+	chrome.contextMenus.update(info.menuItemId, { title: title});
+}
+
+// == Context menu for enabling/disabling reloads. This is across all pages, not just the current page.
+chrome.contextMenus.create({
+	"title": "Disable Auto Reload on Server Change", 
+	"contexts":["page"],
+	"onclick": context_clicked
+}); 
 
 
 
