7ed9e094ff960315018418bda8fbbde95dd263b0
v-a-1 <varun.m.ajani@gmail.com>
2012-09-12 15:15:45 +0530

    Small errors in messaging fixed. Context menu works as before

--- no refresh/bg.js
+++ no refresh/bg.js
@@ -11,39 +11,39 @@ var polling_tab_id = false;
 var enabled = true;
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-
   current_tab_id = activeInfo.tabId;
-  console.log('current_tab_id is ' + current_tab_id);
-  console.log(polling_tab_id);
+  stop_polling_request();
+  start_polling_request();
+});
+
+var stop_polling_request = function(){
   // Tell whichever tab is currently polling to stop.
   if(polling_tab_id != false) {
-    console.log('Trying to send stop_polling to tab with id ' + polling_tab_id);
     chrome.tabs.sendMessage( polling_tab_id, {stop_polling: true}, function(response) {
       if(response.ok == true) {
-       console.log('Received ok to stop_polling command, from tab with id ' + polling_tab_id);
        polling_tab_id = false;
       }
     });   
   }
+}
 
-  // Ask activated tab to start polling provided if not disabled via context menu.
+var start_polling_request = function(){
+  // Ask current tab to start polling.
   if(enabled){
-    console.log('Trying to send start_polling to tab with id ' + current_tab_id);
     chrome.tabs.sendMessage(current_tab_id, {start_polling: true}, function(response) {
       if(response.ok == true) {
         polling_tab_id = current_tab_id;
-        console.log('Received ok to start_polling command, from tab with id ' + current_tab_id);
       }
     });
   }
-
-});
+}
 
 // Callback for context menu click
 var context_clicked = function(info, tab) {
 	enabled = enabled ? false : true;
 	title = enabled ? "Disable Auto Reload on Server Change" : "Enable Auto Reload on Server Change"; 
 	chrome.contextMenus.update(info.menuItemId, { title: title});
+  enabled ? start_polling_request() : stop_polling_request();
 }
 
 // Context menu for enabling/disabling reloads. This is across all pages, not just the current page.
--- no refresh/script.js
+++ no refresh/script.js
@@ -2,13 +2,11 @@ var is_polling = false;
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.stop_polling == true){
-    console.log('received stop_polling');
     if(is_polling){
       stop_polling();
     }
     sendResponse({ok: true});
   }else if(request.start_polling == true){
-    console.log('received start_polling');
     if(!is_polling){
       start_polling();
     }
@@ -18,10 +16,8 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 
 var start_polling = function(){
   is_polling = true;
-  console.log('started_polling');
 }
 
 var stop_polling = function(){
   is_polling = false;
-  console.log('stopped_polling');
 }
