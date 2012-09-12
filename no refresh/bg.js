6de1d2595950c9a43329fe4cce5254a0ec321b3b
v-a-1 <varun.m.ajani@gmail.com>
2012-09-12 21:28:25 +0530

    Implemented everything in bg.js. However due to multiple listeners, the whole thing is a huge mess right now. Still no right architecture

--- no refresh/bg.js
+++ no refresh/bg.js
@@ -1,49 +1,85 @@
-/* Sends start and stop messages to the content script (in each tab matching the manifest.json criteria)
- * depending on whether it is activated or not respectively. It sends a start message only if the user
- * hasn't disabled this, via the context menu. This script also manages what the Context menu text looks
- * like depending on the what is the current toggled state of the enabled variable.
- */
-
 var current_tab_id = 0;
-// Tab that is currently polling.
-var polling_tab_id = false;
-// Var mutated by the contextMenu clicks to provide disabling facility.
-var enabled = true;
 
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-  current_tab_id = activeInfo.tabId;
-  stop_polling_request();
-  start_polling_request();
-});
+// This is so that switching between tabs activates polling.
+chrome.tabs.onActivated.addListener(function(current) {
+  // Keep track of current tab being shown all the time.
+  current_tab_id = current.tabId;
+  chrome.tabs.get(current.tabId, function(tab){
+    console.log(tab.status);
+    if(tab.status == 'complete') {
+      start_polling(tab.url);
+    } // End tab status
+  });// End tabs.get
+}); // End onActivated
 
-var stop_polling_request = function(){
-  // Tell whichever tab is currently polling to stop.
-  if(polling_tab_id != false) {
-    chrome.tabs.sendMessage( polling_tab_id, {stop_polling: true}, function(response) {
-      if(response.ok == true) {
-       polling_tab_id = false;
+// This is so that when a new url is visited in the same tab the polling can accordingly change.
+chrome.tabs.onUpdated.addListener(function(tab_id, info, tab) {
+  chrome.tabs.get(tab_id, function(tab){
+    console.log(tab.status);
+    if(tab.status == 'complete' && tab_id == current_tab_id) {
+      start_polling(tab.url);
+    } // End tab status
+  });// End tabs.get
+}); // End onActivated
+
+var start_polling = function(url){
+  var page;
+
+  // Set to 0 for instant (As long as each xmlhttpRequest takes) reload.
+  // Or set to 4000 (ms) etc. so that there is a minimum gap 
+  // of 4 sec between checks for change.
+  var request_interval = 100;
+  // Slow the request_interval progressively to a certain number of ms. 
+  var slow_down_to = 5000;
+  // Slow the request_interval when tab is inactive to 30s:
+  var inactive_slow_down_to = 30000;
+  // Slow down percentage. 5% is expressed as 0.05;
+  var slow_down_by = 0.05;
+
+  // First make an ajax request and save the page
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      page = xhr.responseText;
+      setTimeout(function(){compare_page()}, request_interval);
+    }
+  }
+  xhr.send();
+
+  // Make another ajax request and compare to first request response.
+  var xhr2 = new XMLHttpRequest();
+  var compare_page = function(){
+    xhr2.open("GET", url, true);
+    xhr2.onreadystatechange = function() {
+      if (xhr2.readyState == 4) {
+        if(xhr2.responseText != page){
+          // If at any point the page changed, reload the page.
+          // location.reload();
+          console.log('server changed.');
+        }else{
+          // Slow down the requests over a period of time to the lowest rate of 5 seconds.
+          if(request_interval < slow_down_to){
+            request_interval += request_interval * slow_down_by;
+          }else{
+            request_interval = slow_down_to;
+          }
+          // Page unchanged. Recurse.
+          setTimeout(function(){compare_page()}, request_interval);
+          console.log('server remains the same.');
+        }
       }
-    });   
+    }
+    xhr2.send();
   }
 }
 
-var start_polling_request = function(){
-  // Ask current tab to start polling.
-  if(enabled){
-    chrome.tabs.sendMessage(current_tab_id, {start_polling: true}, function(response) {
-      if(response.ok == true) {
-        polling_tab_id = current_tab_id;
-      }
-    });
-  }
-}
 
 // Callback for context menu click
 var context_clicked = function(info, tab) {
 	enabled = enabled ? false : true;
 	title = enabled ? "Disable Auto Reload on Server Change" : "Enable Auto Reload on Server Change"; 
 	chrome.contextMenus.update(info.menuItemId, { title: title});
-  enabled ? start_polling_request() : stop_polling_request();
 }
 
 // Context menu for enabling/disabling reloads. This is across all pages, not just the current page.
