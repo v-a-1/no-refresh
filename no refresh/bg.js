01377b19401a9d8b7001ad00235b2bca24b1e284
v-a-1 <varun.m.ajani@gmail.com>
2012-09-12 13:42:41 +0530

    bg.js: rewrite. Changing the whole philosophy of how this works

--- no refresh/bg.js
+++ no refresh/bg.js
@@ -1,34 +1,51 @@
-// All the content scripts loaded in various tabs that match the matched criteria of the manifest.json
-// will constantly ping bg.js asking permission to make XHR requests.
-// What this script does, is checks whether the content script that is asking for permission is running inside
-// a tab that is currently active. Only if that is the case does this grant permission to that particular content script.
+/* Sends start and stop messages to the content script (in each tab matching the manifest.json criteria)
+ * depending on whether it is activated or not respectively. It sends a start message only if the user
+ * hasn't disabled this, via the context menu. This script also manages what the Context menu text looks
+ * like depending on the what is the current toggled state of the enabled variable.
+ */
 
 var current_tab_id = 0;
-
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-  current_tab_id = activeInfo.tabId;
-})
-
+// Hash of tabs that are currently polling.
+var polling_tabs = {}; 
 // Var mutated by the contextMenu clicks to provide disabling facility.
 var enabled = true;
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.wish_to_make_request == true){
-    if(sender.tab.id == current_tab_id && enabled == true){
-      sendResponse({make_request: true});
-    }else{
-      sendResponse({make_request: false});
-    }
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+
+  current_tab_id = activeInfo.tabId;
+  console.log('current_tab_id is '+current_tab_id);
+
+  // Tell all tabs to stop polling
+  for(tab in polling_tabs) {
+    chrome.tabs.sendMessage(current_tab_id, {stop_polling: true}, function(response) {
+      if(response.ok == true) {
+       delete polling_tabs[''+tab+''];
+       console.log('Received ok to stop_polling command, from tab with id '+ tab);
+      }
+    });   
   }
+
+  // Ask activated tab to start polling provided if not disabled via context menu.
+  if(enabled){
+    chrome.tabs.sendMessage(current_tab_id, {start_polling: true}, function(response) {
+      if(response.ok == true) {
+        // Keep track of all polling tabs.
+        polling_tabs[''+current_tab_id+''] = true;
+        console.log('Received ok to start_polling command, from tab with id '+ current_tab_id);
+      }
+    });
+  }
+
 });
 
 // Callback for context menu click
-var context_clicked = function(info, tab){
+var context_clicked = function(info, tab) {
 	enabled = enabled ? false : true;
 	title = enabled ? "Disable Auto Reload on Server Change" : "Enable Auto Reload on Server Change"; 
 	chrome.contextMenus.update(info.menuItemId, { title: title});
 }
 
-// == Context menu for enabling/disabling reloads. This is across all pages, not just the current page.
+// Context menu for enabling/disabling reloads. This is across all pages, not just the current page.
 chrome.contextMenus.create({
 	"title": "Disable Auto Reload on Server Change", 
 	"contexts":["page"],
