77f4ef5c832eb1477fb72ca05e8e74b28eb9b1f8
v-a-1 <varun.m.ajani@gmail.com>
2012-09-12 16:04:34 +0530

    Done. EXCEPT, if you were to load a page from localhost , then navigate away while it was loading, when its done loading it will keep making requests until to activate it and then deactivate and then reactivate. This gets compounded when you switch on chrome and all presaved tabs are loading in the background

--- no refresh/script.js
+++ no refresh/script.js
@@ -1,5 +1,10 @@
 var is_polling = false;
+var page = false; // Page var to be fetched on window load, to use as comparison base.
+var request_interval = 300; // Set to 0 for instant (actually the time XHR takes)
+var slow_down_to = 2000; // Slow down the request_interval to a certain number of ms.
+var slow_down_by = 0.05; // Slow down percentage. 5% is expressed as 0.05;
 
+// Chrome bg.js sends messages to this script asking this to start or stop polling.
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.stop_polling == true){
     if(is_polling){
@@ -14,8 +19,61 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   }
 });
 
+window.onload = function(){
+  // First make an ajax request and save the page
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", document.location.href, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      page = xhr.responseText;
+      setTimeout(function(){start_polling()}, request_interval);
+    }
+  }
+  xhr.send();
+}
+
 var start_polling = function(){
+  if(page == false){
+    // Window hasn't loaded yet and XHR from that hasn't finished.
+    // So recurse 
+    setTimeout(function(){start_polling()}, request_interval);
+    return; //!! MID FUNCTION RETURN
+  }
+  console.log('called start_polling');
   is_polling = true;
+  // Make another ajax request and compare to first request response.
+  var xhr2 = new XMLHttpRequest();
+  var compare_page = function(){
+
+
+    // BREAKS OUT OF RECURSION. !! MID FUNCTION RETURN
+    if(!is_polling){
+      is_polling = false; 
+      return;
+    }
+
+
+    xhr2.open("GET", document.location.href, true);
+    xhr2.onreadystatechange = function() {
+      if (xhr2.readyState == 4) {
+        if(xhr2.responseText != page){
+          location.reload();
+        }else{
+          // Slow down the requests over a period of time to the lowest rate.
+          if(request_interval < slow_down_to){
+            request_interval += request_interval * slow_down_by;
+          }else{
+            request_interval = slow_down_to;
+          }
+          // Page unchanged. Recurse.
+          setTimeout(function(){compare_page()}, request_interval);
+        }
+      }
+    }
+    console.log('making xhr2 request');
+    xhr2.send();
+  }
+  compare_page();
 }
 
 var stop_polling = function(){
