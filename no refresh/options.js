a1eb674511e936caf6002b37151cb1c7e801f772
v-a-1 <varun.m.ajani@gmail.com>
2012-09-23 02:09:52 +0530

    Created options files for settings

---
+++ no refresh/options.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+  restore_options();
+  document.querySelector('button').addEventListener('click', save_options);
+});
+var save_options = function(){
+	localStorage.include_url_patterns = document.getElementById('include_url_patterns').value;
+	localStorage.exclude_url_patterns = document.getElementById('exclude_url_patterns').value;
+	localStorage.ignore_in_page_regex = document.getElementById('ignore_in_page_regex').value;
+};
+var restore_options = function(){
+	if(localStorage.include_url_patterns){
+		document.getElementById('include_url_patterns').value = localStorage.include_url_patterns;
+	}
+	if(localStorage.exclude_url_patterns){
+		document.getElementById('exclude_url_patterns').value = localStorage.exclude_url_patterns;
+	}
+	if(localStorage.ignore_in_page_regex){
+		document.getElementById('ignore_in_page_regex').value = localStorage.ignore_in_page_regex;
+	}
+};
