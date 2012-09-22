4962ad0041bfd9ab1e571ae0cac794cdd68c6fd7
v-a-1 <varun.m.ajani@gmail.com>
2012-09-22 18:52:22 +0530

    Major Rework. Removed extra permissions, fixed multiple listener problems, got context menu working. There is still one BUG: For some reason, context menu disable and enable works the first time. The second time on disabling, it doesn't disable, it continues to run. Can't figure out why. Going out now, so till next weekend

--- no refresh/bg.js
+++ no refresh/bg.js
@@ -1,35 +1,45 @@
-var current_tab_id = 0;
+var current_tab_id;
+// Timeout object that recursively looks for changes.
+// Defined here, so that it can be cleared when a new poll
+// needs to be started on tab switch or tab reload.
+var next_poll;
+
+// Event Listener for tab switching.
+var onActivated = function(current) {
+  // Keep track of current tab being shown.
+  current_tab_id = current.tabId;
+  poll(''); // Clear any previous polls.
+  chrome.tabs.get(current.tabId, function(tab){
+    if(tab.status == 'complete') {
+      poll(tab.url, current_tab_id);
+    }
+  });
+};
+// Event Listener for tab location change.
+var onUpdated = function(tab_id, info, tab) {
+  poll(''); // Clear any previous polls.
+  chrome.tabs.get(tab_id, function(tab){
+    if(tab.status == 'complete' && tab_id == current_tab_id) {
+      poll(tab.url, current_tab_id);
+    }
+  });
+};
 
 // This is so that switching between tabs activates polling.
-chrome.tabs.onActivated.addListener(function(current) {
-  // Keep track of current tab being shown all the time.
-  current_tab_id = current.tabId;
-  chrome.tabs.get(current.tabId, function(tab){
-    console.log(tab.status);
-    if(tab.status == 'complete') {
-      start_polling(tab.url);
-    } // End tab status
-  });// End tabs.get
-}); // End onActivated
+chrome.tabs.onActivated.addListener(onActivated);
+// This is so that when user browses to a new url in the current tab,
+// the polling can accordingly change.
+chrome.tabs.onUpdated.addListener(onUpdated);
 
-// This is so that when a new url is visited in the same tab the polling can accordingly change.
-chrome.tabs.onUpdated.addListener(function(tab_id, info, tab) {
-  chrome.tabs.get(tab_id, function(tab){
-    console.log(tab.status);
-    if(tab.status == 'complete' && tab_id == current_tab_id) {
-      start_polling(tab.url);
-    } // End tab status
-  });// End tabs.get
-}); // End onActivated
-
-var start_polling = function(url){
+var poll = function(url, tab_id){
   var page;
+  if (url === ''){ clearTimeout(next_poll); return; }
 
   // Set to 0 for instant (As long as each xmlhttpRequest takes) reload.
-  // Or set to 4000 (ms) etc. so that there is a minimum gap 
+  // Or set to 4000 (ms) etc. so that there is a minimum gap
   // of 4 sec between checks for change.
   var request_interval = 100;
-  // Slow the request_interval progressively to a certain number of ms. 
+  // Slow the request_interval progressively to a certain number of ms.
   var slow_down_to = 5000;
   // Slow the request_interval when tab is inactive to 30s:
   var inactive_slow_down_to = 30000;
@@ -42,21 +52,21 @@ var start_polling = function(url){
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
       page = xhr.responseText;
-      setTimeout(function(){compare_page()}, request_interval);
+      setTimeout(function(){compare_page();}, request_interval);
     }
-  }
+  };
   xhr.send();
 
   // Make another ajax request and compare to first request response.
   var xhr2 = new XMLHttpRequest();
   var compare_page = function(){
+    if (url === ''){ return; }
     xhr2.open("GET", url, true);
     xhr2.onreadystatechange = function() {
       if (xhr2.readyState == 4) {
         if(xhr2.responseText != page){
           // If at any point the page changed, reload the page.
-          // location.reload();
-          console.log('server changed.');
+          chrome.tabs.reload(tab_id);
         }else{
           // Slow down the requests over a period of time to the lowest rate of 5 seconds.
           if(request_interval < slow_down_to){
@@ -65,29 +75,38 @@ var start_polling = function(url){
             request_interval = slow_down_to;
           }
           // Page unchanged. Recurse.
-          setTimeout(function(){compare_page()}, request_interval);
-          console.log('server remains the same.');
+          next_poll = setTimeout(function(){compare_page();}, request_interval);
         }
       }
-    }
+    };
     xhr2.send();
-  }
-}
-
-
+  };
+};
+//============== Context menu =======================
+var enabled = true;
 // Callback for context menu click
 var context_clicked = function(info, tab) {
-	enabled = enabled ? false : true;
-	title = enabled ? "Disable Auto Reload on Server Change" : "Enable Auto Reload on Server Change"; 
-	chrome.contextMenus.update(info.menuItemId, { title: title});
-}
+  poll(''); // Clear existing polls.
+  if(enabled){
+    chrome.tabs.onActivated.removeListener();
+    chrome.tabs.onUpdated.removeListener();
+    enabled = false;
+  }else{
+    chrome.tabs.onActivated.addListener(onActivated);
+    chrome.tabs.onUpdated.addListener(onUpdated);
+    chrome.tabs.reload(tab.id);
+    enabled = true;
+  }
+  title = enabled ? "Disable Auto Reload on Server Change" : "Enable Auto Reload on Server Change";
+  chrome.contextMenus.update(info.menuItemId, { title: title});
+};
 
 // Context menu for enabling/disabling reloads. This is across all pages, not just the current page.
 chrome.contextMenus.create({
-	"title": "Disable Auto Reload on Server Change", 
+	"title": "Disable Auto Reload on Server Change",
 	"contexts":["page"],
 	"onclick": context_clicked
-}); 
+});
 
 
 
